224d374663086eff96a655c30cba6cab923e35c1
_ <fahmeenmazhar@gmail.com>
2020-04-04 21:44:32 +0500

    Mobile App Development

---
+++ LAB 9/LoginAuthentication/app/src/main/java/fahmeen/lab/loginauthentication/DatabaseHelper.java
@@ -0,0 +1,50 @@
+package fahmeen.lab.loginauthentication;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ProgrammingKnowledge on 4/3/2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "NAME";
+    public static final String COL_2 = "PASSWORD";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (NAME TEXT,PASSWORD TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name,String surname,String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,name);
+        byte[] password = new byte[0];
+        contentValues.put(COL_2, password);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+}
---
+++ LAB 9/LoginAuthentication/app/src/main/java/fahmeen/lab/loginauthentication/MainActivity.java
@@ -0,0 +1,83 @@
+package fahmeen.lab.loginauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editName,editPassword;
+    Button btnviewAll;
+
+    Button btnviewUpdate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText)findViewById(R.id.editText_name);
+        editPassword = (EditText)findViewById(R.id.editText_password);
+        btnviewAll = (Button)findViewById(R.id.button_viewAll);
+        btnviewUpdate= (Button)findViewById(R.id.button_update);
+        viewAll();
+    }
+
+
+    public  void AddData() {
+        View btnAddData = null;
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                                editName.getText().toString(),
+                                editPassword.getText().toString() );
+                        if(isInserted == true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Name :"+ res.getString(0)+"\n");
+                            buffer.append("Password :"+ res.getString(1)+"\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
